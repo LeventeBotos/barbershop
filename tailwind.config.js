8f7e65dd6fdf13d72fd5404aae0138fd3e586433
Levente Botos <66298417+LeventeBotos@users.noreply.github.com>
2023-05-20 19:11:47 +0200

    barbershop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        gray: {
+          "100": "#878282",
+          "200": "#222",
+          "300": "#111827",
+          "400": "rgba(255, 255, 255, 0.07)",
+        },
+        darkslategray: "#333",
+        burlywood: "#d3a27b",
+        white: "#fff",
+      },
+      fontFamily: {
+        "open-sans": "'Open Sans'",
+        ubuntu: "Ubuntu",
       },
     },
+    fontSize: {
+      lg: "18px",
+      "13xl": "32px",
+      base: "16px",
+      "5xl": "24px",
+    },
   },
-  plugins: [],
-}
+  corePlugins: {
+    preflight: false,
+  },
+};
