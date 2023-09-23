493f2292c74477ce0454f2cf2df8ab6f48e7f829
Levente Botos <botos.levente2007@gmail.com>
2023-09-23 11:53:13 +0200

    changed sunm

--- app/S1.tsx
+++ app/S1.tsx
@@ -2,7 +2,7 @@ import { BsStarFill, BsStarHalf } from "react-icons/bs";
 
 export const S1 = () => {
   return (
-    <div className="flex h-min w-full flex-col justify-between md:h-96 md:flex-row">
+    <div className="flex w-full flex-col justify-between gap-5 md:h-96 md:flex-row">
       <div className="flex w-full flex-col justify-evenly text-center text-2xl md:w-2/3">
         <p className="text-4xl font-bold">Barber</p>
         <p>&quot;Budapest legjobb fodrászata&quot;</p>
@@ -16,7 +16,7 @@ export const S1 = () => {
       </div>
       <img
         alt="Kép a fodrászról"
-        className="block h-96 w-full rounded-lg object-cover opacity-60 md:w-1/3"
+        className="block h-80 w-full rounded-lg object-cover opacity-60 md:h-96 md:w-1/3"
         src="/1.jpg"
       />
     </div>
--- app/Services.tsx
+++ app/Services.tsx
@@ -3,40 +3,40 @@ export const Services = () => {
     <div className="flex flex-col p-3 text-center md:p-10">
       <div className="p-5 text-3xl font-bold">Ajánlataink</div>
       <div className="flex flex-col justify-evenly md:flex-row">
-        <div className="w-2/3 self-center md:w-1/3">
-          <div className="card flex flex-col">
-            <img
-              alt="Hajvágás kép"
-              loading="lazy"
-              className=" h-1/2 w-full rounded-t-3xl object-cover"
-              src="https://lh3.googleusercontent.com/p/AF1QipOaG5z2k-xDqQ6j8IEZoxVQhmja5JeyNd83IxCM=s1360-w1360-h1020"
-            />
-            <p className="text-2xl">Hajvágás</p>
-            <p className="text-primary">1500Ft</p>
+        <div className="card flex h-auto w-3/4 flex-col self-center md:w-1/3">
+          <img
+            alt="Hajvágás kép"
+            loading="lazy"
+            className="h-1/2 h-64 w-full rounded-t-lg object-cover md:h-80"
+            src="/2.jpg"
+          />
+          <div className="flex h-20 flex-col justify-center py-10 md:h-32">
+            <p className=" text-2xl md:text-3xl">Hajvágás</p>
+            <p className="text-xl text-primary">1500Ft</p>
           </div>
         </div>
-        <div className="w-2/3 self-center md:w-1/3">
-          <div className="card flex  flex-col">
-            <img
-              alt="Stilizálás kép"
-              loading="lazy"
-              className="h-1/2 w-full rounded-t-3xl object-cover"
-              src="https://lh3.googleusercontent.com/p/AF1QipNsoPrE51B7QTKw_-k4k9YM9LhbIsHSlS2Ql9I2=s1360-w1360-h1020"
-            />
-            <p className=" text-2xl">Stilizálás</p>
-            <p className="text-primary">1500Ft</p>
+        <div className="card flex h-auto w-3/4 flex-col self-center md:w-1/3">
+          <img
+            alt="Stilizálás kép"
+            loading="lazy"
+            className="h-1/2 h-64 w-full rounded-t-lg object-cover md:h-80"
+            src="/3.jpg"
+          />
+          <div className="flex h-20 flex-col justify-center py-10 md:h-32">
+            <p className=" text-2xl md:text-3xl">Stilizálás</p>
+            <p className="text-xl text-primary">1500Ft</p>
           </div>
         </div>
-        <div className="w-2/3 self-center md:w-1/3">
-          <div className="card flex  flex-col">
-            <img
-              alt="Borotválás kép"
-              loading="lazy"
-              className="h-1/2 w-full rounded-t-3xl object-cover"
-              src="https://lh3.googleusercontent.com/p/AF1QipM1MJvEvbM7722YIKCjO37dp2uKTxdvXM1dQIFy=s1360-w1360-h1020"
-            />
-            <p className="text-2xl">Borotválás</p>
-            <p className="text-primary">1500Ft</p>
+        <div className="card flex h-auto w-3/4 flex-col self-center md:w-1/3">
+          <img
+            alt="Borotválás kép"
+            loading="lazy"
+            className="h-1/2 h-64 w-full rounded-t-lg object-cover md:h-80"
+            src="/4.jpg"
+          />
+          <div className="flex h-20 flex-col justify-center py-10 md:h-32">
+            <p className=" text-2xl md:text-3xl">Borotválás</p>
+            <p className="text-xl text-primary">1500Ft</p>
           </div>
         </div>
       </div>
